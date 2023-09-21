40f8c67b41cd894344fc80dffbdf1b2850006afc
Kevsl <ktr2@pm.me>
2023-09-21 09:20:38 +0200

    init

---
+++ MoviesAndCameras/Controller/ViewController.swift
@@ -0,0 +1,65 @@
+
+import UIKit
+
+
+struct Movie: Codable {
+    let Title: String
+    let Year: String
+    let imdbID: String
+    let Poster: String
+}
+
+struct MovieDatas: Codable {
+    let Search: [Movie]
+    let totalResults: String
+    let Response: String
+}
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    var movieDatas: MovieDatas?
+    var moviesList: [Movie] = []
+
+    
+    func getMovies() {
+        guard let searchValue = sitftv.text else {
+            return
+        }
+
+        let urlString = "https://www.omdbapi.com/?s=\(searchValue)&apikey=c6c2a5e9"
+
+        if let url = URL(string: urlString) {
+            URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
+                guard let self = self else { return }
+
+                if let moviesData = data {
+                    do {
+                        let moviesList = try JSONDecoder().decode(MovieDatas.self, from: moviesData)
+                        self.movieDatas = moviesList
+
+                        DispatchQueue.main.async {
+                            self.moviesList =
+                            self.movieDatas?.Search ?? []
+//                            self.setUpTableView()
+                            self.tableView.reloadData()
+
+                        }
+                    } catch {
+                        print(error)
+                    }
+                }
+            }.resume()
+        }
+    }
+}
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
---
+++ MoviesAndCameras/View/MovieTableViewCell.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+
+
+class MovieTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var movieTitle : UILabel!
+    @IBOutlet weak var movieImage: UIImage!
+    
+    var object: Movie!
+
+    func setup(_ newObject: Movie) {
+        object = newObject
+        movieTitle.text = object.Title
+        
+        if let posterURL = URL(string: object.Poster) {
+            URLSession.shared.dataTask(with: posterURL) { [weak self] (data, _, error) in
+                guard let self = self, let data = data, error == nil else {
+                    return
+                }
+                
+                DispatchQueue.main.async {
+                    if let image = UIImage(data: data) {
+                        self.movieImage.image = image
+                    }
+                }
+            }.resume()
+        }
+    }
+}
+
+    
+    
+    
+    
+
+ 
+
+}
